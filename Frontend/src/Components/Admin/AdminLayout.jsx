1b2891282c42703a3d5c4efd0b3c996048fa2e8e
Rohan <rohancjj2003@gmail.com>
2025-09-26 16:19:22 +0530

    import correct css file in admin layout

--- Frontend/src/Components/Admin/AdminLayout.jsx
+++ Frontend/src/Components/Admin/AdminLayout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import AdminSidebar from "./AdminSidebar/AdminSidebar";
 import AdminNavbar from "./AdminNavbar/AdminNavbar";
-import "./AdminLayout.css"; // new file for layout styling
+import "./adminLayout.css"; // new file for layout styling
 
 const AdminLayout = () => {
   return (
